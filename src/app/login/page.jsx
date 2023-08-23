70f74c17483b6fe0e47c38da1498a750adeb56a1
Kibbax <cabello.marcos.ezequiel@gmail.com>
2023-08-23 20:07:24 -0300

    estilado de prueba

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,23 +1,25 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-export default function LoginPage () {
+export default function LoginPage() {
   return (
     <>
-      <form>
-        <label>
+      <form className="max-w-sm mx-auto p-4 border rounded-md shadow-md">
+        <label className="block mb-2">
           Usuario
-          <input type="text" name="username" />
+          <input type="text" name="username" className="w-full px-2 py-1 border rounded" />
         </label>
-        <label>
+        <label className="block mb-2">
           Contraseña
-          <input type="password" name="password" />
+          <input type="password" name="password" className="w-full px-2 py-1 border rounded" />
         </label>
-        <button>Login</button>
+        <button className="bg-blue-500 text-white px-4 py-2 rounded cursor-pointer">
+          Login
+        </button>
         <Link href={'/sign-up'}>Registrarse</Link>
       </form>
       <section>
         <p>video</p>
       </section>
     </>
-  )
+  );
 }
