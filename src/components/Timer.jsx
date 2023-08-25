b085b0fa3462b362f34ad621cd46008a6d1770ab
Alejandro Anibal Isa <alejandroisa71@gmail.com>
2023-08-25 14:55:30 -0300

    Add Folder const and barrel file

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,38 +1,47 @@
-'use client'
+"use client";
 
-import { useState, useEffect } from 'react'
-import { cn } from '@/utils/cn'
+import { useState, useEffect } from "react";
+import { cn } from "@/utils/cn";
 
-const ONE_SECOND_IN_MS = 1000
+const ONE_SECOND_IN_MS = 1000;
 
-export default function Timer ({ title, category, className }) {
-  const [timer, setTimer] = useState(0)
-  const [isRunning, setIsRunning] = useState(false)
+export default function Timer({ title, category, className }) {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
   useEffect(() => {
-    let timerInterval
+    let timerInterval;
 
     if (isRunning) {
       timerInterval = setInterval(() => {
-        setTimer((prev) => prev + 1)
-      }, ONE_SECOND_IN_MS)
+        setTimer((prev) => prev + 1);
+      }, ONE_SECOND_IN_MS);
     } else {
-      clearInterval(timerInterval)
+      clearInterval(timerInterval);
     }
 
-    return () => clearInterval(timerInterval)
-  }, [isRunning])
+    return () => clearInterval(timerInterval);
+  }, [isRunning]);
 
   const handleClick = () => {
-    setIsRunning((prev) => !prev)
-  }
+    setIsRunning((prev) => !prev);
+  };
 
   return (
-    <button className={cn('bg-red-400 rounded-md p-2', className)} onClick={handleClick}>
+    <button
+      className={cn("bg-red-400 rounded-md p-2", className)}
+      onClick={handleClick}
+    >
       <h3>{title}</h3>
       <p>{timer}</p>
-      <span className={cn('bg-red-500 rounded-md p-2', className)}>{category}</span>
-      <img className='w-10 h-10' src="https://images.vexels.com/media/users/3/140908/isolated/lists/bdc30bbe3c022a11e2d7fd0e642c61ae-icono-de-libro-abierto.png" alt='svg icon' />
+      <span className={cn("bg-red-500 rounded-md p-2", className)}>
+        {category}
+      </span>
+      <img
+        className="w-10 h-10"
+        src="https://images.vexels.com/media/users/3/140908/isolated/lists/bdc30bbe3c022a11e2d7fd0e642c61ae-icono-de-libro-abierto.png"
+        alt="svg icon"
+      />
     </button>
-  )
+  );
 }
