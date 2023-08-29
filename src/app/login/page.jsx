8dee648f76ad905ea2a4e3c366cba905fb121702
cristian lell <cristianlell24@gmail.com>
2023-08-29 17:16:58 -0300

    agrego Zustand, Axios y prueba de funcionalidad

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,16 +1,35 @@
+'use client'
+import { useForm } from '@/hooks/useForm'
+import { useUser } from '@/store/useUser'
 import Link from 'next/link'
 
-export default function LoginPage () {
+export default function LoginPage() {
+  const { login } = useUser()
+  const [formLoginValues, handleLoginInputChange] = useForm({
+    username: "",
+    password: "",
+  })
+  const { username, password } = formLoginValues
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const body = {
+      username,
+      password
+    }
+    login(body)
+  }
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Usuario
-          <input type="text" name="username" />
+          <input type="text" name="username" value={username} onChange={handleLoginInputChange} />
         </label>
         <label>
           Contraseña
-          <input type="password" name="password" />
+          <input type="password" name="password" value={password} onChange={handleLoginInputChange} />
         </label>
         <button>Login</button>
         <Link href={'/sign-up'}>Registrarse</Link>
