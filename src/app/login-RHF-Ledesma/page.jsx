7fcaff3ca98aafce158d9a151923641390f02e6f
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-04 14:55:23 -0300

    Se agrega rhf al formulario del login

--- src/app/login-RHF-Ledesma/page.jsx
+++ src/app/login-RHF-Ledesma/page.jsx
@@ -1,23 +1,58 @@
-import Link from 'next/link'
+"use client";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
 
-export default function LoginPage () {
+export default function LoginPage() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <label>
           Usuario
-          <input type="text" name="username" />
+          <input
+            type="text"
+            name="username"
+            {...register("username", {
+              required: "Ingresa un nombre de usuario",
+              maxLength: {
+                value: 20,
+                message: "Maximo 20 caracteres!",
+              },
+            })}
+          />
+          <p className="text-danger">{errors.username?.message}</p>
         </label>
         <label>
           Contraseña
-          <input type="password" name="password" />
+          <input
+            type="password"
+            name="password"
+            {...register("password", {
+              required: "Ingrese su contraseña",
+              maxLength: {
+                value: 16,
+                message: "Maximo 16 caracteres",
+              },
+              pattern: {
+                value: /^(?=.*[A-Z])(?=.*\d).+$/,
+                message:
+                  "La contraseña debe contener al menos una mayuscula y un numero",
+              },
+            })}
+          />
+          <p className="text-danger">{errors.password?.message}</p>
         </label>
         <button>Login</button>
-        <Link href={'/sign-up'}>Registrarse</Link>
+        <Link href={"/sign-up"}>Registrarse</Link>
       </form>
       <section>
         <p>video</p>
       </section>
     </>
-  )
+  );
 }
