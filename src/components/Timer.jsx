b1459b2bc08f205688773d9ceb65f9fa74afc83d
agustin <agustin_correanz@outlook.com>
2023-09-06 14:34:56 -0300

    agregue una carpeta correa-format-hh-mm-ss-timer y agregue un archivo components/Timer.jsx para aplicar la funcion del formato Timer. modifique el archivo src/components/Timer.jsx para hacer el import de FormatToTimer aplicado

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -2,6 +2,8 @@
 
 import { useState, useEffect } from 'react'
 import { cn } from '@/utils/cn'
+import FormatToTimer from '../app/correa-format-HH-MM-SS-timer/timer-components'
+
 
 const ONE_SECOND_IN_MS = 1000
 
@@ -27,18 +29,11 @@ export default function Timer ({ title, category, className }) {
     setIsRunning((prev) => !prev)
   }
 
-    const formatTime = timer => {
-      const hours =Math.floor(timer / 3600);
-      const minutes = Math.floor((timer % 3600) / 60);
-      const seconds = timer % 60;
-
-      return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-    };
 
   return (
     <button className={cn('bg-red-400 rounded-md p-2', className)} onClick={handleClick}>
       <h3>{title}</h3>
-      <p>{formatTime(timer)}</p>
+      <FormatToTimer timer={timer}/>
       <span className={cn('bg-red-500 rounded-md p-2', className)}>{category}</span>
       <img className='w-10 h-10' src="https://images.vexels.com/media/users/3/140908/isolated/lists/bdc30bbe3c022a11e2d7fd0e642c61ae-icono-de-libro-abierto.png" alt='svg icon' />
     </button>
