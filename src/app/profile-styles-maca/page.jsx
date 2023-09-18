321b6a870df1cabefe2df7f87efa8dddd93d1a78
María Macarena Alvarez Castillo <mariamacarenaac@gmail.com>
2023-09-18 16:03:43 -0300

    finishing responsive design

--- src/app/profile-styles-maca/page.jsx
+++ src/app/profile-styles-maca/page.jsx
@@ -1,11 +1,13 @@
+import { FaUserEdit} from 'react-icons/fa';
 
 const ProfilePage = () => {
   return (
-    <div className="min-h-screen bg-gray-100 p-4">
-      <div className="max-w-md mx-auto bg-white p-8 border rounded-lg shadow-lg">
+    <div className="min-h-screen flex items-center justify-center bg-[#242839] p-2">
+
+      <div className="w-11/12 mx-auto bg-white p-8 border rounded-lg shadow-lg md:flex flex-row w-full justify-between lg:flex flex-col items-center w-3/5 xl:w-2/5">
         <div className="text-center">
           <img
-            src="../../assets/profile-picture.jpg"
+            src="https://tse3.mm.bing.net/th?id=OIP.DMDfV6N9jhYjyQCLGLlH5QHaLH&pid=Api&P=0&h=180"
             alt="Foto de perfil"
             className="w-32 h-32 bg-red-200 rounded-full mx-auto"
           />
@@ -13,19 +15,20 @@ const ProfilePage = () => {
         </div>
         <div className="mt-8">
           <h2 className="text-lg font-semibold">Personal Information</h2>
+          <p>Birthday: February 13th </p>
           <p>Location: City, Country</p>
-          <p>Occupation: web Developer</p>
+          <p>Occupation: Web Developer</p>
         </div>
         <div className="mt-8">
           <h2 className="text-lg font-semibold">Recent Timers</h2>
           <ul>
-            <li>Timer 1 - 30 minutes</li>
-            <li>Timer 2 - 45 minutes</li>
+            <li>Timer name - 30 minutes</li>
+            <li>Timer name - 45 minutes</li>
           </ul>
         </div>
-        <div className="mt-8">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
-            Edite Profile
+        <div className="mt-8 md:flex-col items-end">
+          <button className="bg-[#ba8d8a] hover:bg-[#cfb8b7] text-white p-2 rounded-md">
+          <FaUserEdit/>
           </button>
         </div>
       </div>
